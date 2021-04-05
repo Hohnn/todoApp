6413e936debc4e3ccb422796d0ec6eb9f261b1a6
Hohnn <jean-bateee@hotmail.fr>
2021-04-05 22:57:35 +0200

    add remove card script

--- script.js
+++ script.js
@@ -4,11 +4,16 @@ const input = document.getElementById("create")
 console.log(input.value)
 
 function runScript(e) {
-    if (e.keyCode == 13 && input.value != '') {
+    if (e.keyCode == 13 && input.value != '') {        
         let para = document.createElement("div")
-        para.innerHTML = '<div class="newTodo todo show" id="todo1"><button class="state"></button><div class="todoText" ></div></div>'
-        const liste = document.querySelector('.mycard')
-       
+        para.classList = "test"
+        const ggg = document.querySelectorAll(".test")        
+        for (let a = ggg.length; a < ggg.length+1; a++) {
+            para.innerHTML = '<div class="newTodo todo show" id="oui'+a+'"><button class="state"></button><div class="todoText" ></div><div class="suppr"></div></div>'
+            console.log(a)           
+        }       
+        
+        const liste = document.querySelector('.mycard')       
         liste.appendChild(para);
         const tb = document.getElementById("create");
         let value = tb.value;        
@@ -17,13 +22,43 @@ function runScript(e) {
             const element = texte[i];
             element.innerHTML = value
             console.log([i])
+            function click () {
+                const suppr = document.getElementsByClassName('suppr')
+                console.log(suppr)
+
+                for (let i = 0; i < suppr.length; i++) {
+                    suppr[i].onclick = test;                 
+                    };
+
+                function test (event) {
+                    console.log(para)
+                    var target = event.target;
+                    var parent = target.parentElement;
+                    console.log(parent.id)
+                    const jjj = document.getElementById(parent.id)
+                    jjj.classList.toggle('show')
+                   console.log(jjj.classList)
+                }    
+                
+            }     
             
         }
+
         tb.value = '';
+        click();
         
+        return false;        
         
-        console.log(texte)
-        
-        return false;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
