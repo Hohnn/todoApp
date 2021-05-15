b4366fa4c12d8df1863830ca5655e96c70012ce4
Hohnn <jean-bateee@hotmail.fr>
2021-05-15 17:45:30 +0200

    add jquery and sortable

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
-const create = document.getElementById("create")
 const output = document.getElementById("output")
-create.addEventListener('keypress', (e) => {
+$("#create").on('keypress', (e) => {
     if (e.keyCode == 13 && create.value != '') {
         newDiv(create.value)
         create.value = ''
         delTodo()
         completed()
-        allTodo()
-        activeTodo()
-        completedTodo()
-        clearCompleted()
         itemCount()
     }
 })
 
-function newDiv(item1, item2) {
+function newDiv(item1) {
     output.innerHTML += 
-        `<div class="newTodo todo show" data-todo data-active data-text="${item1}">
+        `<div class="newTodo todo" data-todo data-active data-text="${item1}">
             <button class="state" data-button data-value="${item1}"></button>
             <div class="todoText">${item1}</div>
             <div class="suppr" data-delete data-del="${item1}"></div>
@@ -24,94 +19,66 @@ function newDiv(item1, item2) {
 }
 
 function delTodo() {
-    let dataDelete = document.querySelectorAll('div[data-delete]')
-    dataDelete.forEach(element => {
-        element.addEventListener('click', function(){
-            let parentDiv = document.querySelector(`div[data-text="${this.dataset.del}"]`)
-            output.removeChild(parentDiv)
-            itemCount()
-        })
-    })
-}
-
-function completed() {
-    let dataButton = document.querySelectorAll(`button[data-button]`)
-    dataButton.forEach(element => {
-        element.addEventListener('click', function(){
-            let thisButton = document.querySelector(`button[data-value="${this.dataset.value}"]`)
-            thisButton.classList.toggle('complited')
-            thisButton.nextElementSibling.classList.toggle('barré')
-            thisButton.parentNode.toggleAttribute('data-completed')
-            thisButton.parentNode.toggleAttribute('data-active')
-        })
-    })
-}
-
-function allTodo() {   
-    const allButton = document.getElementById("all")
-    allButton.addEventListener('click', ()=> {
-        let allTodo = document.querySelectorAll('div[data-todo]')
-        allTodo.forEach(element => {
-            element.classList.add('show')
-        })
-    })    
-}
-
-function activeTodo() {
-    const activeButton = document.getElementById("active")
-    activeButton.addEventListener('click', ()=>{
-        let activeTodo = document.querySelectorAll('div[data-active]')
-        let completedTodo = document.querySelectorAll('div[data-completed]')
-        completedTodo.forEach(element => {
-            element.classList.remove('show')
-        })
-        activeTodo.forEach(element => {
-            element.classList.add('show')
-        })
-    })
-}
-
-function completedTodo() {
-    const completedButton = document.getElementById("completed")
-    completedButton.addEventListener('click', ()=>{
-        let activeTodo = document.querySelectorAll('div[data-active]')
-        let completedTodo = document.querySelectorAll('div[data-completed]')
-        activeTodo.forEach(element => {
-            element.classList.remove('show')
-        })
-        completedTodo.forEach(element => {
-            element.classList.add('show')
-        })
-    })
-}
-
-function clearCompleted() {
-    const clearButton = document.getElementById("clear")
-    clearButton.addEventListener('click', ()=> {
-        let completedTodo = document.querySelectorAll('div[data-completed]')
-        completedTodo.forEach(element => {
-            output.removeChild(element)
-        })
+    $('div[data-delete]').click( function(){
+        output.remove((`div[data-text="${this.dataset.del}"]`))
         itemCount()
     })
 }
 
-function itemCount() {
-    const items = document.getElementById("items")
-    if (output.children.length > 1) {
-        items.innerHTML = `${output.children.length} items`
-    } else {
-        items.innerHTML = `${output.children.length} item`
-    }
-}
-
-function toggleTheme() {
-    const themeButton = document.getElementById("moonLight")
-    const bodyTheme = document.getElementById("Toggle")
-    themeButton.addEventListener('click', ()=> {
-        bodyTheme.classList.toggle('lightBody')
+function completed() {
+    $(`button[data-button]`).click( function () {
+        let thisButton = document.querySelector(`button[data-value="${this.dataset.value}"]`)
+        let $thisButton = $(`button[data-value="${this.dataset.value}"]`)
+        $thisButton.toggleClass('complited')
+        $thisButton.next().toggleClass('barré')
+        thisButton.parentNode.toggleAttribute('data-completed')
+        thisButton.parentNode.toggleAttribute('data-active')
     })
 }
 
-toggleTheme()
+$(function allTodo() {   
+    $("#all").click( ()=> {
+        $('div[data-todo]').show()
+    })    
+})
 
+$(function activeTodo() {
+    $("#active").click(()=>{
+        $('div[data-completed]').hide()
+        $('div[data-active]').show()
+    })
+})
+
+$(function completedTodo() {
+    $("#completed").click( ()=>{
+        $('div[data-active]').hide()
+        $('div[data-completed]').show()
+    })
+})
+
+$( function clearCompleted() {
+    $('#clear').click( ()=> {
+        $('div[data-completed]').remove()
+        itemCount()
+    })
+})
+
+function itemCount() {
+    const items = $('#items')
+    if (output.children.length > 1) {
+        items.html(`${output.children.length} items`) 
+    } else {
+        items.html(`${output.children.length} item`)
+    }
+}
+
+$( function toggleTheme () {
+    $("#moonLight").click( () => {
+        $("#Toggle").toggleClass('lightBody')
+    } )
+})
+
+$( function sortable () {
+    $( "#output" ).sortable();
+    $( "#output" ).disableSelection();
+  } );
